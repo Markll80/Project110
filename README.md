453fe5e89e7cea5acbda93f5cead67dcf459aed9
Mark Yu <markyu@Markl.local>
2017-02-22 12:17:00 -0800

    message

---
+++ README.md
@@ -0,0 +1,1 @@
+https://travis-ci.org/Markll80/ExcerciseLab3.svg?branch=master
---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,27 @@
+package edu.ucsd.cs110.temperature;
+
+/**
+ * Created by markyu on 2/22/17.
+ */
+public class Celsius extends Temperature{
+    public Celsius(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return null;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return null;
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        return "";
+    }
+}
